703d9766ffa1692cd0559a8fa3ae6cab28821a29
Nasir-Sharif <numl-s22-19947@numls.edu.pk>
2025-10-12 02:08:59 -0700

    Revert "Added a Contact form page"
    This reverts commit 02fedeca0292c34edd3bc8520b4e79a6a4578f7e.

--- lifesync/src/App.jsx
+++ lifesync/src/App.jsx
@@ -5,7 +5,6 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import About from './components/About';
 import Choose from './components/Choose';
-import ContactForm from './components/ContactForm';
 import Services from './components/Services';
 import Footer from './components/Footer';
 import Signup from './components/Signup';
